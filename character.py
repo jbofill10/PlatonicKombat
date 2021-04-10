e6044694273370f926f404cba269d645854a80f7
jbofill10 <juan.bofill10@gmail.com>
2021-04-10 04:03:42 -0400

    Add character class and fix sprites

---
+++ character.py
@@ -0,0 +1,87 @@
+class Character:
+
+    name = None
+    ai = False
+    hp = None
+    current_image = None 
+    pygame = None
+
+    walk = []
+    z_attack = []
+    x_attack = []
+    c_attack = []
+    
+    left = False
+    right = False
+    z_att = False
+    x_att = False
+    c_att = False
+
+    x = 0
+    y = 0
+
+    walkCount = 0
+    z_att_count = 0
+    x_att_count = 0
+    c_att_count = 0
+
+    def __init__(self, name, hp, pygame, x, y, ai=False):
+        self.name = name
+        self.ai = ai
+        self.hp = hp
+        self.pygame = pygame
+        self.x = x
+        self.y = y
+
+        self.load_sprites()
+
+    def load_sprites(self):
+        self.walk = [self.pygame.image.load(f'Sprites/{self.name}/walk/walk0.png'),
+                self.pygame.image.load(f'Sprites/{self.name}/walk/walk1.png'),
+                self.pygame.image.load(f'Sprites/{self.name}/walk/walk2.png')]
+
+        self.z_attack = [self.pygame.image.load(f'Sprites/{self.name}/attacks/z_attack/z_attack0.png')]
+
+        self.x_attack = [self.pygame.image.load(f'Sprites/{self.name}/attacks/x_attack/x_attack1.png')]
+
+        self.current_image = self.pygame.image.load(f'Sprites/{self.name}/player.png')
+    
+    def idle(self):
+        self.left = False
+        self.right = False
+        self.z_att = False
+        self.x_att = False
+        self.c_att = False
+
+        self.walkCount = 0
+        self.z_att_count = 0
+        self.x_att_count = 0
+        self.c_att_count = 0
+
+    def manage_inputs(self):
+        pressed_key = self.pygame.key.get_pressed()
+        
+        if pressed_key[self.pygame.K_d] and self.x <= 1220:
+            self.x+= 1.5 
+            
+            self.right = True
+            self.left = False
+
+        elif pressed_key[self.pygame.K_a] and self.x >= 0:
+            self.x-= 1.5
+            
+            self.left = True
+            self.right = False
+
+        elif pressed_key[self.pygame.K_z]:
+            self.z_att = True
+
+        elif pressed_key[self.pygame.K_x]:
+            self.x_att = True
+
+        else:
+            self.idle()
+
+
+
+    
--- game.py
+++ game.py
@@ -1,5 +1,8 @@
+from character import Character
+
 import pygame
 import sys
+
 
 def main():
     pygame.init()
@@ -8,18 +11,58 @@ def main():
     fpsClock = pygame.time.Clock()
 
     screen = pygame.display.set_mode([1280, 720])
-
-    player = pygame.image.load('Sprites/player.png')
+    
+    x=y=0
+    
+    player = Character('socialmedia_addict', 100, pygame, x, y)
 
     while True:
         screen.fill((255,255,255))
-        screen.blit(player, (0,0))
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
+        for event in pygame.event.get():
+ 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+        
+        player.manage_inputs()
+
+        if player.left or player.right:
+            if player.walkCount > 2:
+            
+                player.walkCount = 0
+
+            if player.right:
+        
+                screen.blit(player.walk[player.walkCount], (player.x,player.y))
+                player.walkCount+=1
+
+            elif player.left:
+
+                screen.blit(pygame.transform.flip(player.walk[player.walkCount], True, False), (player.x, player.y))
+                player.walkCount+=1
+        elif player.z_att:
+
+            if player.z_att_count >= 1:
+                player.z_att_count = 0
+
+            screen.blit(player.z_attack[player.z_att_count] ,(player.x,player.y))
+            
+            player.z_att_count+=1
+
+
+        elif player.x_att:
+            if player.x_att_count >= 1:
+                player.x_att_count = 0
+
+            
+            screen.blit(player.x_attack[player.x_att_count] ,(player.x,player.y))
+
+            player.x_att_count+=1
+
+        else:
+            screen.blit(player.current_image, (player.x,player.y))
+            
         pygame.display.update()
 
 if __name__ == '__main__':
